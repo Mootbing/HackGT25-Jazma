4a5ae85b4b848b5de8bd55140f2e40eca04726b0
AndrewRomitti <andrewromitti450@gmail.com>
2025-09-27 02:43:55 -0400

    Add LLM util function

--- mcp_backend/main.py
+++ mcp_backend/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from git_utils import *
 from repo_utils import *
 from pathlib import Path
+from llm_util import generate_store_payload
 
 app = FastAPI()
 
@@ -9,7 +10,7 @@ app = FastAPI()
 async def process_data(payload, path):
     curr_branch = get_current_branch(path)
     pre_commit_hash = commit_pre_fix_state(path, curr_branch)
-    tmp_branch = create_temp_branch(path, commit_hash)
+    tmp_branch = create_temp_branch(path, pre_commit_hash)
 
     #do changes
     files_to_watch = payload.get("files", [])
@@ -27,11 +28,11 @@ async def process_data(payload, path):
 
     diff_text = git_diff(path, pre_commit_hash, new_commit_hash, files_to_watch)
 
-
     if response.lower() == 'y':
+        generate_store_payload("bug", payload.get("stack_trace", ""), diff_text, payload.get("repo", ""), curr_branch, payload.get("language", ""))
         merge_temp_branch(path, tmp_branch, curr_branch)
     else:
-        rollback_to_commit(path, curr_branch, commit_hash)
+        rollback_to_commit(path, curr_branch, pre_commit_hash)
 
     return {
         "pre_commit": pre_commit_hash,
